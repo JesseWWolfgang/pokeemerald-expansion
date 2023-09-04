c2ff08d79196160a7b29daba7942ffca090019c7
Ultimate Bob <ultimate.bob808@gmail.com>
2023-09-05 00:19:02 +1000

    Better makefile var name

--- porytiles_rules.mk
+++ porytiles_rules.mk
@@ -1,5 +1,5 @@
 # MAKEFLAGS += --no-print-directory
-METATILES_DIR := data/tilesets
+TILESETS_DIR := data/tilesets
 PORYTILES_DIR := data/tilesets/porytiles
 PORYTILES_BINARY := tools/pory/porytiles
 
@@ -10,10 +10,10 @@ DEST_NAMES := tiles.png metatiles.bin metatile_attributes.bin $(PAL_NAMES)
 PRIMARY_TILESET_NAMES := porytiles_test porytiles_test2
 SECONDARY_TILESET_NAMES := porytiles_test_secondary
 
-PRIMARY_TILESET_PATHS := $(patsubst %, $(METATILES_DIR)/primary/%/, $(PRIMARY_TILESET_NAMES))
+PRIMARY_TILESET_PATHS := $(patsubst %, $(TILESETS_DIR)/primary/%/, $(PRIMARY_TILESET_NAMES))
 PRIMARY_TARGETS := $(foreach P, $(PRIMARY_TILESET_PATHS), $(addprefix $(P), $(DEST_NAMES)))
 
-SECONDARY_TILESET_PATHS := $(patsubst %, $(METATILES_DIR)/secondary/%/, $(SECONDARY_TILESET_NAMES))
+SECONDARY_TILESET_PATHS := $(patsubst %, $(TILESETS_DIR)/secondary/%/, $(SECONDARY_TILESET_NAMES))
 SECONDARY_TARGETS := $(foreach P, $(SECONDARY_TILESET_PATHS), $(addprefix $(P), $(DEST_NAMES)))
 
 .PHONY: porytilesall buildprimary buildsecondary
@@ -24,13 +24,13 @@ buildsecondary: $(SECONDARY_TARGETS)
 
 # Primary
 
-$(addprefix $(METATILES_DIR)/primary/porytiles_test/, $(DEST_NAMES)) &: $(shell find $(PORYTILES_DIR)/primary/porytiles_test)
-	$(PORYTILES_BINARY) compile-primary -o $(METATILES_DIR)/primary/porytiles_test $(PORYTILES_DIR)/primary/porytiles_test
+$(addprefix $(TILESETS_DIR)/primary/porytiles_test/, $(DEST_NAMES)) &: $(shell find $(PORYTILES_DIR)/primary/porytiles_test)
+	$(PORYTILES_BINARY) compile-primary -o $(TILESETS_DIR)/primary/porytiles_test $(PORYTILES_DIR)/primary/porytiles_test
 
-$(addprefix $(METATILES_DIR)/primary/porytiles_test2/, $(DEST_NAMES)) &: $(shell find $(PORYTILES_DIR)/primary/porytiles_test2)
-	$(PORYTILES_BINARY) compile-primary -o $(METATILES_DIR)/primary/porytiles_test2 $(PORYTILES_DIR)/primary/porytiles_test2
+$(addprefix $(TILESETS_DIR)/primary/porytiles_test2/, $(DEST_NAMES)) &: $(shell find $(PORYTILES_DIR)/primary/porytiles_test2)
+	$(PORYTILES_BINARY) compile-primary -o $(TILESETS_DIR)/primary/porytiles_test2 $(PORYTILES_DIR)/primary/porytiles_test2
 
 # Secondary
 
-$(addprefix $(METATILES_DIR)/secondary/porytiles_test_secondary/, $(DEST_NAMES)) &: $(shell find $(PORYTILES_DIR)/secondary/porytiles_test_secondary)
-	$(PORYTILES_BINARY) compile-secondary -o $(METATILES_DIR)/secondary/porytiles_test_secondary $(PORYTILES_DIR)/secondary/porytiles_test_secondary $(PORYTILES_DIR)/primary/porytiles_test
+$(addprefix $(TILESETS_DIR)/secondary/porytiles_test_secondary/, $(DEST_NAMES)) &: $(shell find $(PORYTILES_DIR)/secondary/porytiles_test_secondary)
+	$(PORYTILES_BINARY) compile-secondary -o $(TILESETS_DIR)/secondary/porytiles_test_secondary $(PORYTILES_DIR)/secondary/porytiles_test_secondary $(PORYTILES_DIR)/primary/porytiles_test
