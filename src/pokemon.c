c14720784ebdb2aa6595e281ad15b5a57f836729
Ultimate Bob <ultimate.bob808@gmail.com>
2023-10-27 03:43:41 +1100

    Fix legendary battle bgm. Add small wait during trainer slide ins. Fix unova route 17 so you can escape the northern part.

--- src/pokemon.c
+++ src/pokemon.c
@@ -7791,27 +7791,7 @@ u16 GetBattleBGM(void)
         return VarGet(VAR_TEMP_CUSTOM_BATTLE_MUSIC);
     }
 
-    if (gBattleTypeFlags & BATTLE_TYPE_LEGENDARY)
-    {
-        switch (GetMonData(&gEnemyParty[0], MON_DATA_SPECIES, NULL))
-        {
-        case SPECIES_RAYQUAZA:
-            return MUS_VS_RAYQUAZA;
-        case SPECIES_KYOGRE:
-        case SPECIES_GROUDON:
-            return MUS_VS_KYOGRE_GROUDON;
-        case SPECIES_REGIROCK:
-        case SPECIES_REGICE:
-        case SPECIES_REGISTEEL:
-        case SPECIES_REGIGIGAS:
-        case SPECIES_REGIELEKI:
-        case SPECIES_REGIDRAGO:
-            return MUS_VS_REGI;
-        default:
-            return MUS_RG_VS_LEGEND;
-        }
-    }
-    else if (gBattleTypeFlags & (BATTLE_TYPE_LINK | BATTLE_TYPE_RECORDED_LINK))
+    if (gBattleTypeFlags & (BATTLE_TYPE_LINK | BATTLE_TYPE_RECORDED_LINK))
         return MUS_VS_TRAINER;
     else if (gBattleTypeFlags & BATTLE_TYPE_TRAINER)
     {
@@ -7863,11 +7843,24 @@ u16 GetBattleBGM(void)
         #ifndef MUSIC_EXPANSION
             switch (GetMonData(&gEnemyParty[0], MON_DATA_SPECIES, NULL))
             {
+                case SPECIES_RAYQUAZA:
+                    return MUS_VS_RAYQUAZA;
+                case SPECIES_KYOGRE:
+                case SPECIES_GROUDON:
+                    return MUS_VS_KYOGRE_GROUDON;
+                case SPECIES_REGIROCK:
+                case SPECIES_REGICE:
+                case SPECIES_REGISTEEL:
+                case SPECIES_REGIGIGAS:
+                case SPECIES_REGIELEKI:
+                case SPECIES_REGIDRAGO:
+                    return MUS_VS_REGI;
                 case SPECIES_VICTINI:
                 case SPECIES_KELDEO:
                 case SPECIES_MELOETTA:
-                case SPECIES_GENESECT:
                     return MUS_RG_VS_LEGEND;
+                case SPECIES_GENESECT:
+                    return MUS_RG_VS_MEWTWO;
             }
         #endif
         
