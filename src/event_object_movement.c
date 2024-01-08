ae5c95a834dec237ced60e7238f1cf0b7f7a7db6
Ultimate Bob <ultimate.bob808@gmail.com>
2024-01-08 15:59:03 +1100

    Fix sideways stairs merge

--- src/event_object_movement.c
+++ src/event_object_movement.c
@@ -4870,7 +4870,6 @@ static bool8 ObjectEventOnRightSideStair(struct ObjectEvent *objectEvent, s16 x,
 
 u8 GetCollisionAtCoords(struct ObjectEvent *objectEvent, s16 x, s16 y, u32 dir)
 {
-    u8 direction = dir;
     u8 currentBehavior = MapGridGetMetatileBehaviorAt(objectEvent->currentCoords.x, objectEvent->currentCoords.y);
     u8 nextBehavior = MapGridGetMetatileBehaviorAt(x, y);
     u8 collision;
